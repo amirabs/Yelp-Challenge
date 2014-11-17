939f127379c4cb7378bc60fd3550c4da99ce6e21
Amir <amir@DN51skvk.SUNet>
2014-11-16 22:07:57 -0800

    cluster_id added to classes

--- classes.py
+++ classes.py
@@ -3,12 +3,12 @@ class Cluster:
         self.businesses = businesses
 
 class Business:
-    def __init__(self, id_string, review_count, moving_avg_ratings = [],
+    def __init__(self, business_id, review_count, moving_avg_ratings = [],
             categories = [], open_date = -1, closing_date = -1, longitude = 0,
-            latitude = 0, price_range = -1, zip_code = -1):
+            latitude = 0, price_range = -1, zip_code = -1,cluster_id=-1):
         self.review_count = review_count
         self.categories = categories
-        self.id_string = id_string
+        self.business_id = business_id
         self.longitude = longitude
         self.latitude = latitude
         self.open_date = open_date
@@ -17,6 +17,7 @@ class Business:
         self.cat_features = None
         self.zip_code = zip_code
         self.moving_avg_ratings = moving_avg_ratings
+        self.cluster_id=cluster_id
 
     def __str__(self):
-        return "("+str(self.id_string)+","+str(self.review_count)+", opened on: "+str(self.open_date)+", closed on:"+str(self.closing_date)+",price"+str(self.price_range)+")"
+        return "("+str(self.business_id)+","+str(self.review_count)+", opened on: "+str(self.open_date)+", closed on:"+str(self.closing_date)+",price"+str(self.price_range)+")"

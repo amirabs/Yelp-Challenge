54db3979180de51051731e15009fc105cc5437ff
amir <amirabs@stanford.edu>
2014-12-05 00:09:30 -0800

    svm added

---
+++ training.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import mean_squared_error, make_scorer
+from sklearn.cross_validation import KFold, cross_val_score
+from sklearn.linear_model import Lasso
+from sklearn import svm
+from numpy import *
+from pylab import *
+
+# Classes Import
+from classes import *
+from read_data import *
+from clustering import *
+
+def load_file(file_name):
+	arr=np.loadtxt(file_name)
+	return arr
+
+def main3():
+	corr_thresh=0.1;
+	corr_1d=load_file("corr_1d.txt")
+	ind=np.where(np.abs(corr_1d)>corr_thresh)
+	corr_1d_filtered=corr_1d[ind]
+	print corr_1d_filtered.shape
+
+	feature_mat=load_file("feature_mat.txt")
+	feature_mat_filtered=feature_mat[ind]
+
+	print feature_mat_filtered.shape
+
+	cv5 = KFold(len(feature_mat), n_folds=10)
+	lasso = Lasso()
+	mean=np.mean(np.abs(corr_1d))
+	# print mean
+	a=cross_val_score(lasso, feature_mat, corr_1d, cv=cv5, scoring=make_scorer(mean_squared_error))
+	a=np.sqrt(a)/mean
+	print a
+
+def mysvm():
+	corr_thresh=0.1;
+	corr_1d=load_file("corr_1d.txt")
+	ind=np.where(np.abs(corr_1d)>corr_thresh)
+	corr_1d_filtered=corr_1d[ind]
+	ind_bin=np.where(corr_1d_filtered > 0)
+	corr_1d_bin = np.zeros(len(corr_1d_filtered))
+	corr_1d_bin[ind_bin] = 1
+	print corr_1d_bin
+	print corr_1d_filtered
+
+	feature_mat=load_file("feature_mat.txt")
+	feature_mat_filtered=feature_mat[ind]
+
+	print feature_mat_filtered.shape
+
+	clf = svm.SVC(kernel='rbf', C=1)
+	a=cross_val_score(clf, feature_mat_filtered, corr_1d_bin, cv=10)
+	print a
+
+def linear_reg():
+	corr_1d=load_file("corr_1d.txt")
+	# print np.arange(-1,1,0.1)
+	# plt.hist(corr_1d, bins=np.arange(-1,1,0.1))
+	# plt.show()
+	feature_mat=load_file("feature_mat.txt")
+	cv5 = KFold(len(feature_mat), n_folds=10)
+	lasso = Lasso()
+	mean=np.mean(np.abs(corr_1d))
+	# print mean
+	a=cross_val_score(lasso, feature_mat, corr_1d, cv=cv5, scoring=make_scorer(mean_squared_error))
+	a=np.sqrt(a)/mean
+	# print a
+
+if __name__ == "__main__":
+	mysvm()
+
